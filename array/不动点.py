82a1ba39c0b76a513f5f3746f03f8805ff30de76
zyh3826 <31238754+zyh3826@users.noreply.github.com>
2019-06-14 17:02:47 +0800

    Update 不动点.py

--- array/不动点.py
+++ array/不动点.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def fixedPoint(self, A: List[int]) -> int:
+        fixed_point = [x for x in A if x == A.index(x)]
+        if len(fixed_point) == 0:
+            return -1
+        return fixed_point[0]
