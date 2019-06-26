eefa30a91500a015903ec2d1d79973b7004444b4
zyh3826 <31238754+zyh3826@users.noreply.github.com>
2019-06-26 21:06:48 +0800

    Create 移除元素.py

---
+++ array/移除元素.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        try:
+            while True:
+                nums.remove(val)
+                
+        except ValueError:
+            return len(nums)
