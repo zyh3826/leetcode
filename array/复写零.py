a5298636a81dc52e56838652d0423466bf2425a4
zyh3826 <31238754+zyh3826@users.noreply.github.com>
2019-07-03 21:59:14 +0800

    Create 复写零.py

---
+++ array/复写零.py
@@ -0,0 +1,13 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        length = len(arr)
+        i = 0
+        while i < length:
+            if arr[i] == 0:
+                arr.insert(i+1, 0)
+                arr.pop()
+                i += 1
+            i += 1
